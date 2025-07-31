6fd4085c26d41584e1bcdd67635f16db058738a2
Begench2506 <bega2500601@gmail.com>
2025-07-31 16:09:53 +0300

    .env and models.py added

---
+++ app/models.py
@@ -0,0 +1,14 @@
+import os
+from app import db
+from datetime import datetime
+
+class TempData(db.Model):
+    __tablename__ = 'temperature_data'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sensor_id = db.Column(db.Integer)
+    tvalue = db.Column(db.Float)
+    datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<MusTun id={self.id}, sensor_id={self.sensor_id}, tvalue={self.tvalue}, datetime={self.datetime}>'
