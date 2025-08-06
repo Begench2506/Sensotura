ca21ce22a0facceb35d374c3be646132f506bd55
Begench2506 <Bega2500601@gmail.com>
2025-08-06 17:52:45 +0300

    Updates

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import text
 from models import db, TempData
 from config import DevelopmentConfig 
+from datetime import datetime, timedelta
 import os
 
 app = Flask(__name__)
@@ -12,15 +13,24 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    try:
-        with app.app_context():
-            db.session.execute(text("SELECT 1"))
-            print("Подключение к БД успешно!")
-            data = TempData.query.limit(10).all()
-            return render_template('index.html', data=data)
-    except Exception as e:
-        print(f"Ошибка БД: {str(e)}")
-        return render_template('index.html', data=None)
+    now = datetime.utcnow()
+    yesterday = now - timedelta(hours=24)
+
+    recent_data = (
+        TempData.query
+        .filter(TempData.datetime >= yesterday)
+        .order_by(TempData.datetime)
+        .all()
+    )
+
+    chart_data = [
+        {"sensor_id": row.sensor_id, "tvalue": row.tvalue, "datetime": row.datetime.strftime('%Y-%m-%d %H:%M')}
+        for row in recent_data
+    ]
+
+    print(chart_data)
+
+    return render_template('index.html', data=chart_data)
 
 
 if __name__ == '__main__':
